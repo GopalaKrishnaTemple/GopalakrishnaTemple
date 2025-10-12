0d662ab89958825b886c54961c2f819a5fe6b6a8
Anshu Shetty <anshushetty2003@gmail.com>
2025-10-12 11:53:23 +0530

    added temple cms strapi

---
+++ backend/temple-cms/config/plugins.ts
@@ -0,0 +1,12 @@
+module.exports = {
+  graphql: {
+    enabled: true,
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 10,
+      amountLimit: 100,
+    },
+  },
+};
